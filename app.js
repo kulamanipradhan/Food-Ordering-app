83fae704906d57a1db8ee26418fa85ffab20db7b
kulamanipradhan <km99pradhan@gmail.com>
2023-12-15 08:14:04 +0530

    html is very complex not that easy

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+/*
+
+ <div id="parent">
+      <div id="child">
+        <h1></h1>
+      </div>
+    </div>
+
+
+
+*/
+const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, React.createElement("h1", { id: "parent" }, "hello world")))
+
 const heading = React.createElement("h1", { id: "heading" }, "Hello world from React");
+console.log(heading)//object
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(heading);
